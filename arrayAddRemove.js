8382937942fd7035149cfd494d24ab8c72d725f9
Sohidul Islam <sishufol.sim@gmail.com>
2022-04-22 22:03:29 +0600

    array push and pop

---
+++ arrayAddRemove.js
@@ -0,0 +1,15 @@
+var bookpage = [
+    "hablu",
+    "bolod",
+    "chouddogosti",
+    "battery",
+    "peracitamol",
+    "gymnasium",
+];
+
+bookpage.push("dhaka");
+bookpage.push("bangladesh");
+bookpage.push("chattogram");
+console.log(bookpage);
+bookpage.pop();
+console.log(bookpage);
