ec65f0a7dea7037c37c7a247e4ce525005a09db5
Sohidul Islam <sishufol.sim@gmail.com>
2022-04-22 21:15:59 +0600

    1st commit

---
+++ floatP1.js
@@ -0,0 +1,8 @@
+const Name = "Sohidul Islam"
+console.log(Name);
+
+var num1 = 0.1
+var num2 = 0.2
+var num3 = num1 + num2
+console.log("sum of two numbers : ", num3);
+console.log("type of number 3:  ", typeof num3);
