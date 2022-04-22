4d57a16e979b63ce11741e1f9534207e072b07b6
Sohidul Islam <sishufol.sim@gmail.com>
2022-04-22 21:19:47 +0600

    set 2 decimal points

--- floatP1.js
+++ floatP1.js
@@ -6,3 +6,4 @@ var num2 = 0.2
 var num3 = num1 + num2
 console.log("sum of two numbers : ", num3);
 console.log("type of number 3:  ", typeof num3);
+console.log("Set two decimal points", num3.toFixed(2))
