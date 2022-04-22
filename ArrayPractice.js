1ba87fe8582fa4f4dabb61a44ffb417d1ce6cc96
Sohidul Islam <sishufol.sim@gmail.com>
2022-04-22 21:39:10 +0600

    array declaration and initializaiton

---
+++ ArrayPractice.js
@@ -0,0 +1,13 @@
+// Array declaration
+// by using square 
+const arr = [11, 13, 17]
+
+console.log(arr);
+
+var murichanachurFee = [34, 55, 221]
+
+console.log("murichanacur fees", murichanachurFee);
+
+
+var naikas = ["mahi", "opu", "sabana", "kopila"]
+console.log(naikas);
