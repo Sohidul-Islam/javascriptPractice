26aa70c2655f7140c8e04db31956ab94107660c9
Sohidul Islam <sishufol.sim@gmail.com>
2022-04-22 21:48:54 +0600

    array indexOf function

---
+++ arrayIndex.js
@@ -0,0 +1,8 @@
+var bookpage = ["hablu", "bolod", "chouddogosti", "battery", "peracitamol", "gymnasium"]
+var batteryIndex = bookpage.indexOf("battery")
+console.log(batteryIndex);
+var batteryIndex = bookpage.indexOf("bolod")
+console.log(batteryIndex);
+var numbers = [45, 98, 65, 72, 52, 63];
+var index = numbers.indexOf(52)
+console.log(index);
