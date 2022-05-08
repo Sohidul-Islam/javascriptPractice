98b5db35cf9efbc3e98a1b4291d2b0b469fa4e55
Sohidul Islam <sishufol.sim@gmail.com>
2022-05-09 00:52:45 +0600

    object chaining

--- es6/arrayDestructuring.js
+++ es6/arrayDestructuring.js
@@ -3,6 +3,8 @@
 const myObject = { x: 2, y: 50, z: 600, a: 25, b: 68 }
 const { x, b, a } = myObject;
 console.log("Destructring: ", x);
+console.log("Destructring: ", b);
+console.log("Destructring: ", a);
 
 // Destructring array 
 
@@ -13,3 +15,22 @@ const [t, ...u] = [90, 70, 10, 84]
 console.log("Array Destructring: ", p, " and ", q);
 console.log("Array Destructring: ", r, " and ", s);
 console.log("Array Destructring: ", t, " and ", u);
+
+const myObject2 = {
+
+    name: "Sohidul Islam",
+    id: "CSE01806649",
+    dept: {
+        building: ["A", "B", "C", "D", "E", "F"],
+        deptName: ["CSE", "BBA", "EEE"],
+        studentNumber: [45, 60, 40]
+    },
+    courses: {
+        majorCourse: ["A", "B", "C", "D"],
+        nonmajorCourse: ["E", "F", "G"]
+    }
+}
+//Object chaining
+console.log(myObject2.courses.majorCourse);
+console.log(myObject2?.courses?.majorCourse);
+console.log(myObject2.course?.majorCourse);
