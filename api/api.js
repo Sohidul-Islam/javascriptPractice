914aa4e3427c83f1d34e4f9591e66ce110f76ed3
Sohidul Islam <sishufol.sim@gmail.com>
2022-05-10 16:13:38 +0600

    convert Json to stringify object

--- api/api.js
+++ api/api.js
@@ -14,3 +14,16 @@ const getPost = () => {
         .then(response => response.json())
         .then(json => console.log(json))
 }
+
+const std = {
+    name: "shufol",
+    id: 123,
+    dept: "CSE"
+}
+
+console.log("Json object ", std);
+
+const StringiFy = JSON.stringify(std);
+console.log("Json to stringify object: ", StringiFy);
+const JsonObject = JSON.parse(StringiFy);
+console.log("Stringify to Json object: ", JsonObject);
